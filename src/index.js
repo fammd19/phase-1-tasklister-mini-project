bea8045c8f4644cca74870cd8b4e57a386162a0f
Fiona Mellink <fimellink@gmail.com>
2024-01-01 12:51:33 +1100

    Complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector('#create-task-form');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    buildToDo(e.target['new-task-description'].value);
+    form.reset();
+  })
 });
+
+function buildToDo (todo) {
+  let li = document.createElement ('li');
+  li.textContent = todo;
+  document.querySelector('#tasks').appendChild(li);
+}
+
